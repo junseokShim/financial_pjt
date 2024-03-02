e2251a8614be377943ccc7b02f320a2ee1629ea0
unknown <tjdrbsrhk123@github.com>
2024-03-02 09:28:53 +0900

    add resampling

--- src/efficient_frontier.py
+++ src/efficient_frontier.py
@@ -17,6 +17,9 @@ class EfficientFontier:
         self.expected_returns   = None   # 기대 수익률
         self.covariance_matrix  = None   # 공분산 행렬
 
+        self.resampled_expected_returns     = None
+        self.resampled_covariance_matrix    = None
+
         self.RETURN_INDEX           = 0
         self.VOLATILITYU_INDEX      = 1
         self.SHARP_INDEX            = 2
@@ -72,12 +75,43 @@ class EfficientFontier:
         return self.get_statistics(weights)[self.VOLATILITYU_INDEX]
     
 
-    def optimize_portpolio_about_returns(self, drop_col_list, min_return, max_return, sample_num):
+    def resampling_ret_vols(self, size, N_path=50, N_point=50, resampling=True):
+        # 자산 계수
+        N = len(self.tickers)
+
+        if resampling:
+            # 리샘플링을 위한 시뮬레이션
+            ret_vec_stack = np.zeros((N_path, N))
+            cov_mat_stack = np.zeros((N_path, N, N))
+
+            for i in range(N_path):
+                # 리샘플링 경로 생성
+                # 다변항 정규분포로부터 임의의 난수 추출
+                data = np.random.multivariate_normal(self.expected_returns, self.covariance_matrix, size=size)
+
+                # 새롭게 만들어진 기대 수익률 벡터
+                ret_vec_i = data.mean(axis=0)
+                ret_vec_stack[i, :] = ret_vec_i
+
+                # 새롭게 만들어진 공분산 행렬
+                cov_mat_i = np.cov(data.T)
+                cov_mat_stack[i, :, :] = cov_mat_i
+
+            # 새로 업데이트된 기대 수익률 벡터와 공분산 행렬
+            self.expected_returns     = ret_vec_stack.mean(axis=0)
+            self.covariance_matrix    = cov_mat_stack.mean(axis=0)
+
+
+    def optimize_portpolio_about_returns(self, drop_col_list, min_return, max_return, sample_num, resampling=True):
         '''
         수익률 수준별 포트폴리오 최적화 수행
+        
+        resampling : 투자 포트폴리오 성능 예측에 있어, 불확실성과 과적합을 줄이기 위해 사용
         '''
         self.get_etf_data(drop_col_list)
         self.get_optimize_model_inputs()
+
+        self.resampling_ret_vols(size=self.WORKING_DAYS_PER_YEAR, resampling=resampling)
 
         t_rets = np.linspace(min_return, max_return, sample_num)
         t_vols = list()
